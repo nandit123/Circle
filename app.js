17a9dc8c67ea7721199ac71e8c3bab53b0f30635
Nandit Mehra <nandit.mehra.15csc@bml.edu.in>
2020-05-02 18:10:26 +0530

    excel file upload and reading complete

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var xlstojson = require("xls-to-json-lc");
+var xlsxtojson = require("xlsx-to-json-lc");
+var fs = require('fs');
+
+router.use(bodyParser.json());
+
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function (req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        var datetimestamp = Date.now();
+        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1])
+    }
+});
+
+var upload = multer({ //multer settings
+    storage: storage,
+    fileFilter : function(req, file, callback) { //file filter
+        if (['xls', 'xlsx'].indexOf(file.originalname.split('.')[file.originalname.split('.').length-1]) === -1) {
+            return callback(new Error('Wrong extension type'));
+        }
+        callback(null, true);
+    }
+}).single('file');
+
+/** API path that will upload the files */
+router.post('/upload', function(req, res) {
+    var exceltojson; //Initialization
+    upload(req,res,function(err){
+        if(err){
+             res.json({error_code:1,err_desc:err});
+             return;
+        }
+        /** Multer gives us file info in req.file object */
+        if(!req.file){
+            res.json({error_code:1,err_desc:"No file passed"});
+            return;
+        }
+        //start convert process
+        /** Check the extension of the incoming file and
+         *  use the appropriate module
+         */
+        if(req.file.originalname.split('.')[req.file.originalname.split('.').length-1] === 'xlsx'){
+            exceltojson = xlsxtojson;
+        } else {
+            exceltojson = xlstojson;
+        }
+        try {
+            exceltojson({
+                input: req.file.path, //the same path where we uploaded our file
+                output: null, //since we don't need output.json
+                lowerCaseHeaders:true
+            }, function(err,result){
+                if(err) {
+                    return res.json({error_code:1,err_desc:err, data: null});
+                }
+                try {
+                    fs.unlinkSync(req.file.path);
+                } catch(e) {
+                    console.log('Could not delete the uploaded file');
+                }
+                res.json({error_code:0,err_desc:null, data: result});
+            });
+        } catch (e){
+            res.json({error_code:1,err_desc:"Corupted excel file"});
+        }
+    });
+});
+
+
+router.get("/", (req, res) => {
+    res.sendFile((__dirname+'/View/home.html'));
+});
+
+
+router.get("/excel", (req, res) => {
+    res.sendFile((__dirname+'/View/excel.html'));
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express();
+const main = require('./app')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+app.use("/", main);
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
 });
